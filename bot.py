6433b4efd3c3f09e38a9cf71b6a63c81be629eaf
python0208 <96980515+python0208@users.noreply.github.com>
2022-03-20 11:48:29 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,11 +16,11 @@ from nonebot.adapters.onebot.v11 import Adapter as ad
 nonebot.init()
 app = nonebot.get_asgi()
 nonebot.load_plugins('src/plugins')
-nonebot.init(apscheduler_autostart=True)
-nonebot.init(apscheduler_config={
-    "apscheduler.timezone": "Asia/Shanghai"
-})
-nonebot.load_plugins('src/plugins/foo')
+# nonebot.init(apscheduler_autostart=True)
+# nonebot.init(apscheduler_config={
+#     "apscheduler.timezone": "Asia/Shanghai"
+# })
+nonebot.load_plugins('src/plugins/foo') # 这是插件的文件路，你可以跟着创建 plugins是你创建好项目后自动生成，只要在plugins里面创建个foo文件就行，在foo文件里面放py文件（只是我这里是这样创建的，你可以随意，主要得有plugins文件夹在里面）
 driver = nonebot.get_driver()
 driver.register_adapter(ad)
 #
